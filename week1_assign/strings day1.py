884a7c90452f93102a26eec3655ba2f23c8d0316
Berlin alusi <64525487+Alusi-aligura@users.noreply.github.com>
2025-04-13 23:57:02 +0300

    Add Hangman game

--- week1_assign/strings day1.py
+++
@@ -1,1 +0,0 @@
-print("baddies code")
---
+++ week2_classes/strings_day1.py
@@ -0,0 +1,20 @@
+# sequence of characters
+
+greetings = "Hello world"
+print(greetings)
+
+#concurtinate
+f_name = "Ivy"
+l_name = "Berlin"
+
+full_name = f_name + "" + l_name
+#print(full_name)
+
+#repeat_names = f_name * 3
+#print(repeaat_names)
+
+#slicing and indexing
+print(l_name[1:3])
+
+#negative indexing
+print(l_name[:-7])
